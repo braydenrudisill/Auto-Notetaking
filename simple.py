c4e6cd3b23934dafeedf8f9f6d73bbdab677249f
Brayden Rudisill <braydenrudisill@gmail.com>
2021-07-19 15:11:38 -0700

    Upload to github

---
+++ simple.py
@@ -0,0 +1,53 @@
+from __future__ import absolute_import
+from __future__ import division, print_function, unicode_literals
+
+from sumy.parsers.html import HtmlParser
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.nlp.stemmers import Stemmer
+from sumy.utils import get_stop_words
+
+
+
+def summarize_text(TXT,LANGUAGE,SENTENCES_COUNT): 
+    parser = PlaintextParser.from_string(TXT, Tokenizer(LANGUAGE))
+    stemmer = Stemmer(LANGUAGE)
+
+    summarizer = Summarizer(stemmer)
+    summarizer.stop_words = get_stop_words(LANGUAGE)
+
+    for sentence in summarizer(parser.document, SENTENCES_COUNT):
+        print(sentence)
+        
+def transcribe_file(speech_file):
+    """Transcribe the given audio file."""
+    from google.cloud import speech
+    import io
+
+    client = speech.SpeechClient()
+
+    with io.open(speech_file, "rb") as audio_file:
+        content = audio_file.read()
+
+    audio = speech.RecognitionAudio(content=content)
+    config = speech.RecognitionConfig(
+        language_code="en-US",
+    )
+
+    response = client.recognize(config=config, audio=audio)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+##    print(response)
+##    print(response.results)
+    output = ""
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        output += result.alternatives[0].transcript + ","
+    return output
+
+txt = transcribe_file("dest.flac")
+print(txt)
+print("\n------SUMMARY-------\n")
+summarize_text(txt,"english",10)     # prints summary
